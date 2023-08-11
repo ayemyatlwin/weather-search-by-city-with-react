cdc4d3322b6f21405087444b273109300a1ff836
Hannah <erikahn05@gmail.com>
2023-08-12 01:20:58 +0630

    added

--- src/Srce.jsx
+++ src/Srce.jsx
@@ -4,7 +4,7 @@ const Srce = () => {
   return (
     <h1 className='text-center text-sm text-gray-500'>
       source code 
-      <a href="https://github.com/ayemyatlwin/weather-search-by-city-with-react.git" className='text-blue-500'> here</a>
+      <a target='_blnk' href="https://github.com/ayemyatlwin/weather-search-by-city-with-react.git" className='text-blue-500'> here</a>
 
     </h1>
   )
